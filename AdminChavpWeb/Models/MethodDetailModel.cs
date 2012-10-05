4467935c130fec49eab08920d2b539ae2a4e4abc
chavp <my.parinya@gmail.com>
2012-10-05 15:13:38 +0700

    plus MethodDetailModel.cs
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ AdminChavpWeb/Models/MethodDetailModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminChavpWeb.Models
+{
+    public class MethodDetailModel
+    {
+        public DateTime Date { get; set; }
+
+        public string MachineName { get; set; }
+        public string ServiceName { get; set; }
+        public string MethodName { get; set; }
+
+        public int TotalUsage { get; set; }
+
+        public double TotalElapsedMilliseconds { get; set; }
+        public double AverageElapsedMilliseconds { get; set; }
+
+    }
+}
